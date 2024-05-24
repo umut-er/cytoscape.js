39bfb20fccbc62459478f834be4c070e92890817
Umut Utku ERŞAHİNCE <umut-er@Umuts-MBP-2.home>
2024-05-24 21:44:30 +0300

    sbgn.js implementations added

--- src/math.js
+++ src/math.js
@@ -1106,4 +1106,10 @@ math.getBarrelCurveConstants = function( width, height ){
 };
 
 
+math.calculateDistance = function (point1, point2) {
+  var distance = Math.pow(point1[0] - point2[0], 2) + Math.pow(point1[1] - point2[1], 2);
+  return Math.sqrt(distance);
+};
+
+
 module.exports = math;
